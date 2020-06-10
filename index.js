4a6fb105d0bd1bd566906339edfd291eb8cf3c35
Aditya Nalini <adinalini2000@gmail.com>
2020-06-11 02:01:57 +0530

    Added /username task

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-    res.send("Hello world!");
+    res.send("Hello!");
 });
+
+app.get('/user/:username', function(req, res) {
+    res.send("Hello " + req.params.username);
+});
+
+
 
 app.listen(3000);
